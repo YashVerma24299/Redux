9e9df54e5f75bb65d95955a000c77eb0db20ddd8
Yash <ritikverma23@gmail.com>
2025-06-09 17:30:42 +0530

    Added a functionality to delete a task in list.

--- Redux_Todo/src/components/Todo.jsx
+++ Redux_Todo/src/components/Todo.jsx
@@ -1,16 +1,23 @@
-import { useSelector } from "react-redux"
+import { useSelector, useDispatch } from "react-redux"
 import AddForm from "./AddForm";
+import { deleteTodo } from "../features/todo/todoSlice";
 
 export default function Todo(){
     const todos = useSelector((state) => state.todos);
     console.log(todos);
+    const dispatch = useDispatch();
+
+    let handleDelete =(id)=>{
+        console.log("delete", id);
+        dispatch(deleteTodo(id));
+    }
     return(
         <>
             <AddForm/>
             <h2>Todos</h2>
             <ul>
                 {todos.map((todo) => (
-                    <li key={todo.id}>{todo.task}</li>
+                    <li key={todo.id}>{todo.task} &nbsp; &nbsp; <button onClick={()=> handleDelete(todo.id)}>delete</button></li>   
                 ))}
             </ul>
         </>
