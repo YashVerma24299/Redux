b88bfb7ccd76552f2c470d558178ef9ab1f59d23
Yash <ritikverma23@gmail.com>
2025-06-09 12:21:28 +0530

    Applied Provider Component and make a TodoList.

--- Redux_Todo/src/App.jsx
+++ Redux_Todo/src/App.jsx
@@ -1,9 +1,17 @@
+// Provider component:
+    // It makes the Redux store available to your entire application.
 
+import Todo from './components/Todo';
+import {Provider} from 'react-redux'
+import {store} from "./app/store"
 
 function App() {
   return (
     <>
-        gf
+      {/* Ye jo provider hai "todo" ko "store" dega aur agr todo ki trh 10 aur ho unko bhi sore dega... */}
+      <Provider store={store}> {/* It makes the Redux store available to all nested component */}
+        <Todo/>
+      </Provider>
     </>
   )
 }
